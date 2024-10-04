8428ddd44664fead66f199d481bdfff668c146c7
Federico Moya <federicoamoya@gmail.com>
2024-10-04 09:58:39 -0300

    make fake.Event implement Event interface

--- fake/event.go
+++ fake/event.go
@@ -1,42 +1,47 @@
 package fake
 
 import (
-    "context"
+	"context"
+	"encoding/json"
 
-    "github.com/walletera/message-processor/errors"
-    "github.com/walletera/message-processor/events"
+	"github.com/walletera/message-processor/errors"
+	"github.com/walletera/message-processor/events"
 )
 
 var _ events.Event[EventVisitor] = Event{}
 
 type Event struct {
+	FakeID              string `json:"id"`
+	FakeType            string `json:"type"`
+	FakeCorrelationID   string `json:"correlation_id"`
+	FakeDataContentType string `json:"data_content_type"`
+	FakeData            string `json:"data"`
 }
 
 func (f Event) ID() string {
-    //TODO implement me
-    panic("implement me")
+	return f.FakeID
 }
 
 func (f Event) Type() string {
-    //TODO implement me
-    panic("implement me")
+	return f.FakeType
 }
 
 func (f Event) CorrelationID() string {
-    //TODO implement me
-    panic("implement me")
+	return f.FakeCorrelationID
 }
 
 func (f Event) DataContentType() string {
-    //TODO implement me
-    panic("implement me")
+	return f.FakeDataContentType
 }
 
 func (f Event) Serialize() ([]byte, error) {
-    //TODO implement me
-    panic("implement me")
+	serialized, err := json.Marshal(f)
+	if err != nil {
+		return nil, err
+	}
+	return serialized, nil
 }
 
 func (f Event) Accept(ctx context.Context, visitor EventVisitor) errors.ProcessingError {
-    return visitor.VisitFakeEvent(ctx, f)
+	return visitor.VisitFakeEvent(ctx, f)
 }
